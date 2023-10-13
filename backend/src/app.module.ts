fb03d9c9ecb201b99e8a6f0b583e760343c6a12f
joaocarvoli <j@>
2023-10-13 11:46:12 -0300

    Removing unecessary code

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import {FirebaseController} from "./firebase.controller";
 import {FirebaseService} from "./firebase.service";
 
 @Module({
   imports: [],
-  controllers: [AppController, FirebaseController],
-  providers: [AppService, FirebaseService],
+  controllers: [ FirebaseController],
+  providers: [ FirebaseService],
 })
 export class AppModule {}
--- backend/src/firebase.controller.ts
+++ backend/src/firebase.controller.ts
@@ -1,5 +1,4 @@
 import {Controller, Get} from "@nestjs/common";
-import {AppService} from "./app.service";
 import {FirebaseService} from "./firebase.service";
 
 @Controller("firebase")
